e54b5f117a56ed89ae0db7e12184495e6c3080d9
Graves Theroux <gravesth@un-m1.kzn.21-school.ru>
2026-04-20 01:33:00 +0300

    flag -n

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -8,36 +8,33 @@ int main(int argc, char *argv[]) {
     // 0 - имя исполняемого файла
     // 1 - cat или grep 
     // 2 - флаг
-    
     if(argv[1][0] == '-') {
         switch(argv[1][1]) {
-        case 'b': 
-            if (number_nonblank(argv[2], line)) { 
-                return 0; 
-            }
-            break;
-        case 'E': 
-            if (e_gnu_flag(argv[2], line)) { 
-                return 0; 
-            }
-            break; 
-        case 'e': 
-            if (e_posix_flag(argv[2], line)) { 
-                return 0; 
-            }
-            break; 
-        // case 'n':
+        case 'b': if (number_nonblank(argv[2], line)) return 0; break;
+        case 'E': if (e_gnu_flag(argv[2], line)) return 0; break; 
+        case 'e': if (e_posix_flag(argv[2], line)) return 0; break;  
+        case 'n': if (number(argv[2], line)) return 0; break;
         // case 's':
         // case 't':
         } 
-    } else {  
-        if (none_flags(argv[1], line)) { 
-            return 0; 
-        } 
-    }
-
+    } else { if (none_flags(argv[1], line)) return 0; }
 }
 
+// Flag -n
+int number(char *str, char *line) { 
+    FILE *file = fopen(str, "r"); 
+    int count = 0;
+    if (file == NULL) {
+        printf("%s","n/a");
+        return 0; 
+    }
+    while(fgets(line, 1024, file) != NULL) { 
+        count++; 
+        printf("%6d  %s", count, line);
+    }
+    fclose(file);
+    return 1; 
+}
 // Flag -b
 int number_nonblank(char *str, char *line) {
     FILE *file = fopen(str, "r"); 
@@ -63,25 +60,8 @@ int number_nonblank(char *str, char *line) {
     fclose(file);
     return 1; 
 } 
-int none_flags(char *str, char *line) { 
-    FILE *file = fopen(str, "r"); 
-    if (file == NULL) {
-        printf("%s","n/a");
-        return 0; 
-    }
-
-    while(fgets(line, 1024, file) != NULL) { 
-        printf("%s", line); 
-    }
-    // line - буффер. 
-    // читаем не более 1024 символов
-    // входной поток
-
-    fclose(file);
-    return 1;
-}
-
-// Реализация флага -E без -v (GNU only)
+// None flag
+// Реализация флага -E без -v (GNU only). 
 // Отображаем только $ в конце
 int e_gnu_flag (char *str, char *line) { 
     FILE *file = fopen(str, "r"); 
@@ -99,7 +79,6 @@ int e_gnu_flag (char *str, char *line) {
     fclose(file); 
     return 1; 
 }
-
 // Реализация флага -е без -v (POSIX)
 // Отображаем все управляющие символы
 int e_posix_flag(char *str, char *line) {
@@ -120,6 +99,46 @@ int e_posix_flag(char *str, char *line) {
             }
         }
     }
+    fclose(file);
+    return 1;
+}
+
+int squeeze_blank(char *str, char *line){ 
+    FILE *file = fopen(str, "r");
+    if (file == NULL) {
+        printf("n/a");
+        return 0;
+    }
+    while (fgets(line, 1024, file)) {
+        for (int i = 0; line[i] != '\0'; i++) {
+            char ch = line[i]; 
+            if (ch == '\n') {
+                printf("$\n");
+            } else if (ch == '\r') {
+                printf("^M");
+            } else {
+                putchar(ch);
+            }
+        }
+    }
+    fclose(file);
+    return 1;
+}
+
+int none_flags(char *str, char *line) { 
+    FILE *file = fopen(str, "r"); 
+    if (file == NULL) {
+        printf("%s","n/a");
+        return 0; 
+    }
+
+    while(fgets(line, 1024, file) != NULL) { 
+        printf("%s", line); 
+    }
+    // line - буффер. 
+    // читаем не более 1024 символов
+    // входной поток
+
     fclose(file);
     return 1;
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -5,5 +5,6 @@ int none_flags(char *str, char *line);
 int number_nonblank(char *str, char *line);
 int e_posix_flag(char *str, char *line);
 int e_gnu_flag (char *str, char *line);
-
+int number(char *str, char *line);
+int squeeze_blank(char *str, char *line); 
 
