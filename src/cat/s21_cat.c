ae82a7a0338bf89ebf3f58c617096525da3dbbe4
Graves Theroux <gravesth@un-m1.kzn.21-school.ru>
2026-04-20 01:12:14 +0300

    flags -e -E

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,9 +1,6 @@
-#include <stdio.h> 
-#include <string.h> 
-// ./main -b "text.txt"
+#include "s21_cat.h" 
 
-int none_flags(char *str, char *line); 
-int number_nonblank(char *str, char *line);
+// ./main -b "text.txt"
 
 int main(int argc, char *argv[]) {
     char line[1024]; 
@@ -11,20 +8,30 @@ int main(int argc, char *argv[]) {
     // 0 - имя исполняемого файла
     // 1 - cat или grep 
     // 2 - флаг
-
+    
     if(argv[1][0] == '-') {
         switch(argv[1][1]) {
         case 'b': 
-            if(number_nonblank(argv[2], line)) { 
+            if (number_nonblank(argv[2], line)) { 
                 return 0; 
             }
             break;
+        case 'E': 
+            if (e_gnu_flag(argv[2], line)) { 
+                return 0; 
+            }
+            break; 
+        case 'e': 
+            if (e_posix_flag(argv[2], line)) { 
+                return 0; 
+            }
+            break; 
         // case 'n':
         // case 's':
         // case 't':
         } 
     } else {  
-        if(none_flags(argv[1], line)) { 
+        if (none_flags(argv[1], line)) { 
             return 0; 
         } 
     }
@@ -55,8 +62,7 @@ int number_nonblank(char *str, char *line) {
 
     fclose(file);
     return 1; 
-}
-
+} 
 int none_flags(char *str, char *line) { 
     FILE *file = fopen(str, "r"); 
     if (file == NULL) {
@@ -74,3 +80,47 @@ int none_flags(char *str, char *line) {
     fclose(file);
     return 1;
 }
+
+// Реализация флага -E без -v (GNU only)
+// Отображаем только $ в конце
+int e_gnu_flag (char *str, char *line) { 
+    FILE *file = fopen(str, "r"); 
+    if(file == NULL) {
+        printf("%s", "n/a");
+        return 0;
+    }
+    while(fgets(line, 1024, file)) { 
+        size_t len = strlen(line);
+        if (len > 0 && line[len-1] == '\n') {
+            line[len-1] = '\0';
+        }
+        printf("%s$\n", line);
+    }
+    fclose(file); 
+    return 1; 
+}
+
+// Реализация флага -е без -v (POSIX)
+// Отображаем все управляющие символы
+int e_posix_flag(char *str, char *line) {
+    FILE *file = fopen(str, "r");
+    if (file == NULL) {
+        printf("n/a");
+        return 0;
+    }
+    while (fgets(line, 1024, file)) {
+        for (int i = 0; line[i] != '\0'; i++) {
+            char ch = line[i]; 
+            if (ch == '\n') {
+                printf("$\n");
+            } else if (ch == '\r') {
+                printf("^M");
+            } else {
+                putchar(ch);
+            }
+        }
+    }
+    fclose(file);
+    return 1;
+}
+
---
+++ src/cat/s21_cat.h
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <string.h>
+
+int none_flags(char *str, char *line); 
+int number_nonblank(char *str, char *line);
+int e_posix_flag(char *str, char *line);
+int e_gnu_flag (char *str, char *line);
+
+
